a6442f58a2f76b509dd67eae0da909794e6762dd
Abhinav <athawait5@gmail.com>
2017-09-01 11:52:02 +0530

    first commit

---
+++ smartlog2/src/main/java/user/itjunkies/com/smartlog/SmartLog.java
@@ -0,0 +1,15 @@
+package user.itjunkies.com.smartlog;
+
+import android.util.Log;
+
+/**
+ * Created by User on 01-Sep-17.
+ */
+
+public class SmartLog {
+    static final String TAG = "smart_log";
+
+    public static void d(String msg) {
+        Log.d(TAG, msg);
+    }
+}
