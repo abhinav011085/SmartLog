4c5dc9d662b146412b907b940d3124c8e2e0dea4
Abhinav <athawait5@gmail.com>
2017-09-06 10:57:35 +0530

    added image picker

---
+++ smartlog2/src/main/java/user/itjunkies/com/smartlog/SquareImageView.java
@@ -0,0 +1,30 @@
+package user.itjunkies.com.smartlog;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by User on 06-Sep-17.
+ */
+
+public class SquareImageView extends android.support.v7.widget.AppCompatImageView {
+
+    public SquareImageView(final Context context) {
+        super(context);
+    }
+
+    public SquareImageView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(final Context context, final AttributeSet attrs, final int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        final int width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        setMeasuredDimension(width, width);
+    }
+}
