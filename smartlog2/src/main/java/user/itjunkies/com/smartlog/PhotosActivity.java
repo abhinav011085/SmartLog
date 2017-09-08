d2c88d30af53007743f2e67d63cdb5d619a0038a
Abhinav <athawait5@gmail.com>
2017-09-08 16:42:33 +0530

    commit

--- smartlog2/src/main/java/user/itjunkies/com/smartlog/PhotosActivity.java
+++ smartlog2/src/main/java/user/itjunkies/com/smartlog/PhotosActivity.java
@@ -1,7 +1,14 @@
 package user.itjunkies.com.smartlog;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -9,10 +16,14 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PhotosActivity extends AppCompatActivity {
     Context context = this;
@@ -47,6 +58,19 @@ public class PhotosActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(context, 3));
         recyclerView.addItemDecoration(new ItemDecorationAlbumColumns(10, 3));
         recyclerView.setAdapter(new AdapterPhotos(context, FolderActivity.al_images, int_position));
+
+        ImageView take_pic= (ImageView) findViewById(R.id.take_pic);
+        take_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(PhotosActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+                } else {
+                    takePicture();
+                }
+            }
+        });
     }
 
     @Override
@@ -58,9 +82,49 @@ public class PhotosActivity extends AppCompatActivity {
             intent.putStringArrayListExtra(new ImagePicker().DATA, PhotosActivity.selected_images);
             setResult(RESULT_OK, intent);
             finish();
+        } else if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                selected_images.add(file.getPath());
+                Intent intent = new Intent(context, ShowPhotosActivity.class);
+                startActivityForResult(intent, new ImagePicker().IMAGEPICHER_REQ);
+            }
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                takePicture();
+            }
+        }
+    }
+
+    Uri file;
+
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+        startActivityForResult(intent, 100);
+    }
+
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
     @Override
     public void onBackPressed() {
         super.onBackPressed();
